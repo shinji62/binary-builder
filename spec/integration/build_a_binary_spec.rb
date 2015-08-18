7d664066d3dbf82eee50ab318bd4fd706347370c
Amin Jams <aminjam.software@gmail.com>
2015-08-18 18:05:37 -0400

    Adding checksum verifier support for Python [#101447594]
    Signed-off-by: Jordan Collier <jordanicollier@gmail.com>

--- spec/integration/build_a_binary_spec.rb
+++ spec/integration/build_a_binary_spec.rb
@@ -3,12 +3,13 @@ require 'fileutils'
 
 describe 'building a binary', :integration do
   before do
-    output, _ = run_binary_builder(binary_name, binary_version)
+    output, _ = run_binary_builder(binary_name, binary_version, checksum)
   end
 
   context 'when node is specified', binary: 'node' do
     let(:binary_name) { 'node' }
     let(:binary_version) { '0.12.2' }
+    let(:checksum) { "don'tcare" }
 
     it 'builds the specified binary, tars it, and places it in your current working directory' do
       binary_tarball_location = File.join(Dir.pwd, 'node-0.12.2-linux-x64.tar.gz')
@@ -25,13 +26,14 @@ describe 'building a binary', :integration do
 
   context 'when ruby is specified', binary: 'ruby' do
     let(:binary_name) { 'ruby' }
-    let(:binary_version) { '2.0.0-p645' }
+    let(:binary_version) { '2.2.0' }
+    let(:checksum) { 'cd03b28fd0b555970f5c4fd481700852' }
 
     it 'builds the specified binary, tars it, and places it in your current working directory' do
-      binary_tarball_location = File.join(Dir.pwd, 'ruby-2.0.0-p645-linux-x64.tgz')
+      binary_tarball_location = File.join(Dir.pwd, 'ruby-2.2.0-linux-x64.tgz')
       expect(File).to exist(binary_tarball_location)
 
-      ruby_version_cmd = %q{./spec/assets/binary-exerciser.sh ruby-2.0.0-p645-linux-x64.tgz ./bin/ruby -e 'puts RUBY_VERSION'}
+      ruby_version_cmd = %q{./spec/assets/binary-exerciser.sh ruby-2.2.0-linux-x64.tgz ./bin/ruby -e 'puts RUBY_VERSION'}
       output, status = run(ruby_version_cmd)
 
       expect(status).to be_success
@@ -42,10 +44,11 @@ describe 'building a binary', :integration do
 
   context 'when jruby is specified', binary: 'jruby' do
     let(:binary_name) { 'jruby' }
-    let(:binary_version) { '9.0.0.0.rc2_ruby-2.2.0' }
+    let(:binary_version) { '9.0.0.0-2.2.0' }
+    let(:checksum) { '270ebfba1bb57aca3b41a98aadd53549' }
 
     it 'builds the specified binary, tars it, and places it in your current working directory' do
-      binary_tarball_location = File.join(Dir.pwd, 'jruby-9.0.0.0.rc2_ruby-2.2.0-linux-x64.tgz')
+      binary_tarball_location = File.join(Dir.pwd, 'jruby-9.0.0.0-2.2.0-linux-x64.tgz')
       expect(File).to exist(binary_tarball_location)
 
       jruby_version_cmd = %q{./spec/assets/jruby-exerciser.sh}
@@ -60,6 +63,7 @@ describe 'building a binary', :integration do
   context 'when python is specified', binary: 'python' do
     let(:binary_name) { 'python' }
     let(:binary_version) { '3.4.3' }
+    let(:checksum) { "don'tcare" }
 
     it 'builds the specified binary, tars it, and places it in your current working directory' do
       binary_tarball_location = File.join(Dir.pwd, 'python-3.4.3-linux-x64.tgz')
@@ -77,6 +81,7 @@ describe 'building a binary', :integration do
   context 'when httpd is specified', binary: 'httpd' do
     let(:binary_name) { 'httpd' }
     let(:binary_version) { '2.4.12' }
+    let(:checksum) { 'b8dc8367a57a8d548a9b4ce16d264a13' }
 
     it 'builds the specified binary, tars it, and places it in your current working directory' do
       binary_tarball_location = File.join(Dir.pwd, 'httpd-2.4.12-linux-x64.tgz')
@@ -95,6 +100,7 @@ describe 'building a binary', :integration do
   context 'when php is specified', binary: 'php' do
     let(:binary_name) { 'php' }
     let(:binary_version) { '5.6.9' }
+    let(:checksum) { '561f37377833772ace776143c5687884' }
 
     it 'builds the specified binary, tars it, and places it in your current working directory' do
       binary_tarball_location = Dir.glob(File.join(Dir.pwd, 'php-5.6.9-linux-x64-*.tgz')).first
